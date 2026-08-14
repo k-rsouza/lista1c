a18ba97a0143b5569c2e432186c9b996d4f13188
k-rsouza <k.souza@edu.pucrs.br>
2026-08-13 21:33:13 -0300

    first commit

---
+++ ex1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double a, b, c;
+    scanf("%lf %lf %lf", &a, &b, &c);
+
+    double delta = b * b - 4 * a * c;
+
+    if (delta < 0) {
+        printf("0\n");
+    } else if (delta == 0) {
+        double x = -b / (2 * a);
+        printf("1 %.4f\n", x);
+    } else {
+        double x1 = (-b + sqrt(delta)) / (2 * a);
+        double x2 = (-b - sqrt(delta)) / (2 * a);
+        printf("2 %.4f %.4f\n", x1, x2);
+    }
+
+    return 0;
+}
